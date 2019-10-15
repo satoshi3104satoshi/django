5dc97ca28b4b826c9aaee2c2c14661c290fb691e
3104k <3104k@gamil.com>
2019-10-15 16:32:55 +0900

    second

--- manager_project/manager/models.py
+++ manager_project/manager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+
+    HOKKAIDO = 0
+    TOHOKU = 5
+    TOKYO = 10
+    CHIBA = 11
+    KANAGAWA = 12
+    SAITAMA = 13
+    TOCHIGI = 14
+    IBARAGI = 15
+    CHUBU = 20
+    KANSAI = 25
+    CHUGOKU = 30
+    SHIKOKU = 35
+    KYUSHU = 40
+    OKINAWA = 45
